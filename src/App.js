3824d5eb065ec224245202879e81bdbcc5e92c9e
Tanmay Tiwari <thetanmaytiwari@gmail.com>
2026-05-04 22:02:54 +0530

    working

--- src/App.js
+++ src/App.js
@@ -1,33 +1,11 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-
-const Person = (props) =>{
-  return(
-    <>
-    <h1>Name: {props.name}</h1>
-    <h2>Age: {props.age}</h2>
-    </>
-  );
-}
+import React from 'react';
 
 const App = () => {
-  
-  const [counter, setCounter]=useState(0);
-
-  useEffect(()=>{
-    alert("hello maalik")
-
-  },[counter])
-
-  return (
-    <div className="App">
-      <>
-      <button onClick={() => setCounter((prevCount)=> prevCount - 1)}>-</button>
-      <h1>{counter}</h1>
-      <button onClick={() => setCounter((prevCount)=> prevCount + 1)}>+</button>
-      </>
-    </div>
-  );
+    return(
+        
+        <h1>fetching...</h1>
+        
+    );
 }
 
 export default App;
