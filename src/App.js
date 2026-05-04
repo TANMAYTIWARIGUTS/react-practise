c6d6f3dcbaba31787af894624ccfe27e21ebe35a
Tanmay Tiwari <thetanmaytiwari@gmail.com>
2026-05-04 17:50:50 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const name='John';
+  const isNameShowing=false;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{isNameShowing?(
+        <>
+        hello
+        123
+        </>):(
+         <>
+        <p>check</p>
+          <p>check2</p>
+        </>
+          
+          )}, fetching info...</h1>
     </div>
   );
 }
